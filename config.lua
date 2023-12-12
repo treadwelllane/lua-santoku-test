26361c60875daea75b6e9a2135bc3be25f08fa24
Matthew Brooks <mb964033@gmail.com>
2023-12-12 20:51:30 +0000

    bugfix in rockspec

--- config.lua
+++ config.lua
@@ -9,7 +9,7 @@ local env = {
   dependencies = {
     "lua >= 5.1",
     "santoku >= 0.0.143-1",
-    "santoku-system >= 0.0.1-1"
+    "santoku-system >= 0.0.1-1",
     "santoku-fs >= 0.0.1-1"
   },
 
