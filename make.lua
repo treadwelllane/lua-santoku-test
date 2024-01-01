3d8f8798a1709fe141a274c4beacc015399cf621
Matthew Brooks <mb964033@gmail.com>
2024-01-01 18:06:58 +0000

    add missing santoku dependency

--- make.lua
+++ make.lua
@@ -1,13 +1,14 @@
 local env = {
 
   name = "santoku-test",
-  version = "0.0.6-1",
+  version = "0.0.7-1",
   variable_prefix = "TK_TEST",
   license = "MIT",
   public = true,
 
   dependencies = {
     "lua >= 5.1",
+    "santoku >= 0.0.158-1",
   },
 
 }
@@ -17,5 +18,6 @@ env.tarball = env.name .. "-" .. env.version .. ".tar.gz"
 env.download = env.homepage .. "/releases/download/" .. env.version .. "/" .. env.tarball
 
 return {
+  type = "lib",
   env = env
 }
